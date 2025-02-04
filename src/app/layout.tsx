fa968415ff17ebdaf629b6cb2f379e17095b6d41
Kimdev254 <bonifacekimani715@gmail.com>
2025-02-05 00:47:16 +0300

    navbar initial setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
+import Navbar from "@/components/Navbar"
 import type React from "react" // Added import for React
 
 const inter = Inter({ subsets: ["latin"] })
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Navbar/>
         {children}
       </body>
     </html>
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,36 @@
+import Link from "next/link"
+import "../app/globals.css"
+
+import { Inter } from "next/font/google"
+import { link } from "fs"
+
+const inter = Inter({ subsets: ["latin"] })
+
+export const metadata = {
+  title: "Navbar Practice",
+  description: "A simple Next.js app with a navbar",
+}
+
+const links = [
+  {href: "/", label: "Home"},
+  {href: "/about", label: "Docs"},
+  {href: "/contact", label:"Contact"}
+]
+
+const Navbar = () => {
+  return (
+    <header>
+      <nav>
+        <ul className="flex items-center">
+          {links.map(link =>{
+            <li key={link.href}>
+              <Link href={link.href}>{link.label}</Link>
+            </li>
+          })}
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+export default Navbar
